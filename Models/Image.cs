6cc4cd28e6379573f70c83e2f665ac50bed022af
Huy Nguyễn <huyhong2k2oppa@gmail.com>
2024-11-06 19:04:27 +0700

    Update model

---
+++ Models/Image.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace basic_api.Models
+{
+    [Table("images")]
+    public class Image
+    {
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("image_url")]
+        public string ImageURL { get; set; } = string.Empty;
+
+        [ForeignKey("images_orders")]
+        [Column("order_id")]
+        public int? OrderID { get; set; }
+
+        [Column("created_at")]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        [Column("updated_at")]
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+        public Order? Order { get; set; }
+    }
+}
---
+++ Models/Message.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace basic_api.Models
+{
+    [Table("messages")]
+    public class Message
+    {
+        [Column("id")]
+        public int Id { get; set; }
+
+
+        [ForeignKey("messages_chats")]
+        [Column("chat_id")]
+        public int? ChatID { get; set; }
+
+        [ForeignKey("messages_users")]
+        [Column("sender_id")]
+        public int? SenderID { get; set; }
+
+        [Column("text")]
+        public string Text { get; set; } = string.Empty;
+
+        [Column("created_at")]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        [Column("updated_at")]
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+        public Chat? Chat { get; set; }
+    }
+}
