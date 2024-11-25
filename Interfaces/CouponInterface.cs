fe2b2aa926e484166e7015ab6027f5b41cee8da7
Huy Nguyễn <huyhong2k2oppa@gmail.com>
2024-11-25 17:33:17 +0700

    Update Interfaces

---
+++ Interfaces/CouponInterface.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+using basic_api.Models;
+
+namespace basic_api.Interfaces
+{
+    public interface ICouponInterface
+    {
+        Task<Coupon> Create(Coupon coupon);
+        Task<List<Coupon>> GetAll(Expression<Func<Coupon, bool>> predicate);
+        Task<Coupon?> Update(Coupon coupon);
+        Task<Coupon?> GetCouponById(int id);
+    }
+}
---
+++ Interfaces/PunishmentInterface.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+using basic_api.Data;
+using basic_api.Models;
+
+namespace basic_api.Interfaces
+{
+    public interface IPunishmentInterface
+    {
+        Task<Punishment> Create(ApplicationDBContext? context, Punishment punishment);
+        Task<List<Punishment>> GetAll();
+        Task<Punishment?> TakePunishment(Expression<Func<Punishment, bool>> predicate);
+        Task<Punishment?> Delete(int id);
+        Task<Punishment?> Update(Punishment punishment);
+    }
+}
