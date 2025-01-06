fce830f718a30435e9e7cfafe95c000cdd1d3d02
quoc030502 <quocdolce@gmail.com>
2025-01-06 20:51:39 +0700

    update

--- constants/Status.cs
+++ constants/Status.cs
@@ -5,9 +5,16 @@
         public const string New = "New";
         public const string PendingConfirm = "PendingConfirm";
         public const string OrderSuccess = "OrderSuccess";
+        public const string PendingReturn = "PendingReturn";
         public const string Returning = "Returning";
-        public const string ReturnSuccess = "ReturnSuccess";
+        public const string Success = "Success";
         public const string Canceled = "Canceled";
+    }
+
+    public class OrderImageType
+    {
+        public const string Confirm = "Confirm";
+        public const string Return = "Return";
     }
 }
 
