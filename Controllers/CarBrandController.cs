e606bfa93377e63015529327bb90f0591e8d7c20
Huy Nguyễn <huyhong2k2oppa@gmail.com>
2025-01-09 20:05:05 +0700

    Feat: Update

--- Controllers/CarBrandController.cs
+++ Controllers/CarBrandController.cs
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 ﻿using basic_api.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using basic_api.Data;
@@ -49,56 +49,5 @@ namespace basic_api.Controllers
         }
     }
 }
-=======
+
 ﻿
-using basic_api.Interfaces;
-using Microsoft.AspNetCore.Mvc;
-using basic_api.Data;
-using basic_api.Dtos;
-using basic_api.Models;
-using basic_api.Middlewares;
-
-namespace basic_api.Controllers
-{
-    [Route("api/admins/car-brands")]
-    [ApiController]
-    public class AdminCarBrandController(ICarBrandInterface carBrandRepo) : ControllerBase
-    {
-        private readonly ICarBrandInterface _carBrandRepo = carBrandRepo;
-
-        [HttpGet()]
-        [IsAdmin]
-        public async Task<IActionResult> GetList()
-        {
-            var carBrands = await _carBrandRepo.GetAll();
-            return Ok(carBrands);
-        }
-
-        [HttpDelete("{id}")]
-        [IsAdmin]
-        public async Task<IActionResult> Delete([FromRoute] int id)
-        {
-            var carBrand = await _carBrandRepo.Delete(id);
-
-            if (carBrand == null) return NotFound(ErrorMessages.UserNotFound);
-
-            return Ok();
-        }
-
-
-        [HttpPost()]
-        [IsAdmin]
-        public async Task<IActionResult> Create([FromBody] CreateCarBrandRequest req)
-        {
-            var carBrand = await _carBrandRepo.Create(
-              new CarBrand
-              {
-                  Brand = req.Brand,
-              }
-            );
-
-            return Ok(carBrand);
-        }
-    }
-}
->>>>>>> 90ce7f10766621f0afaee5aee687c9b45c201bb1
